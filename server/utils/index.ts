325e0ec29c068b766cda9354314d84f40b51d00f
daniel413x <62479098+daniel413x@users.noreply.github.com>
2023-06-27 23:02:41 -0400

    fix issues in server json util

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,6 +1,6 @@
-/* eslint-disable */
+// eslint-disable-next-line import/prefer-default-export
 export const handleGptJson = (text: string) => {
-  // gpt-3.5-turbo avoids most json problems to begin with, but some problem categories can be offenders, particularly "String"
+  // gpt-3.5-turbo avoids most json problems to begin with, but some problem categories can be offenders, particularly "String", thus the utility of this func
   let newText = text.split('\n').join('');
   newText = newText.split('\\n').join('');
   // remove \n lines
@@ -8,6 +8,7 @@ export const handleGptJson = (text: string) => {
   // remove "Here's a unique LeetCode problem blah blah..."
   const match = newText.match(removeUselessText);
   if (match) {
+    // eslint-disable-next-line prefer-destructuring
     newText = match[0];
   } else {
     return '';
@@ -24,15 +25,14 @@ export const handleGptJson = (text: string) => {
   }
   newText = newText.replace(/\\n/g, '');
   newText = newText.replace(/\\"/g, "'");
-  const missingProp = /:\s*,/.test(newText)
+  const missingProp = /:\s*,/.test(newText);
   if (missingProp) {
     newText = newText.replace(/":\s*,"\s*/g, '": "",');
   }
-  const unevenQuots = newText.match(/"/g)[0].length % 2 > 0;
-  // uneven quotes can happen in problems where one of the answers is an empty string 
+  const unevenQuots = newText.match(/"/g).length % 2 > 0;
+  // uneven quotes can happen in problems where one of the answers is an empty string
   if (unevenQuots) {
-    newText = newText.replace(/":\s*("|([^"]*)"|"([^"]*))\s*(,\s*"|\}(?!;))/g, '": "$2$3"$4');
-    console.log(newText);
+    newText = newText.replace(/":\s*("|([^"]*)"|"([^"]*))\s*(,\s*")/g, '": "$2$3"$4');
     // target invalid json props like { "inputText": ", "inputText": d"...}
   }
   return newText;
